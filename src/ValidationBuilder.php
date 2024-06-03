f490047de3553af7479b9a621f08b2c28051d678
webhkp <webhkp@gmail.com>
2024-06-03 17:26:40 +0600

    Refactor ValidationBuilder and included tests for the builder

--- src/ValidationBuilder.php
+++ src/ValidationBuilder.php
@@ -14,7 +14,7 @@ class ValidationBuilder {
      *
      * @var array Array of ValidationRule
      */
-    private static array $rules = [];
+    private array $rules = [];
 
     private const RULE_NAMESPACE = 'Webhkp\Pvalidate\Rules';
 
@@ -22,40 +22,49 @@ class ValidationBuilder {
 
     }
 
-    public function __call(string $name, array $arguments): static {
+    public function __call(string $name, array $arguments): self {
+        return self::processValidationBuilding($name, $arguments, $this);
+    }
+
+    public static function __callStatic(string $name, array $arguments): self {
         return self::processValidationBuilding($name, $arguments);
     }
 
-    public static function __callStatic(string $name, array $arguments): static {
-        return self::processValidationBuilding($name, $arguments);
-    }
-
-    private static function processValidationBuilding(string $functionName, array $arguments): static {
-        if (self::$builder === null) {
-            self::$builder = new ValidationBuilder();
+    private static function processValidationBuilding(string $functionName, array $arguments, ?ValidationBuilder $instance = null): self {
+        if (!$instance) {
+            $instance = new ValidationBuilder();
         }
 
         $rule = self::RULE_NAMESPACE . '\\' . ucfirst($functionName);
 
         if (class_exists($rule)) {
-            self::$rules[$functionName] = new $rule(...$arguments);
+            $instance->rules[$functionName] = new $rule(...$arguments);
+        } else {
+            throw new \Exception("Rule not found: {$rule}");
         }
 
-        return self::$builder;
+        return $instance;
     }
 
     public function parse(mixed $data): ValidationResponse {
+        return $this->processParsing($data);
+    }
+
+    public function safeParse(mixed $data): ValidationResponse {
+        return $this->processParsing($data, true);
+    }
+
+    private function processParsing(mixed $data, bool $markSafe = false): ValidationResponse {
         $validationResult = [];
 
-        foreach (self::$rules as $key => $rule) {
-            $parseResult = $rule->safeParse($data);
+        foreach ($this->rules as $key => $rule) {
+            $parseResult = $markSafe ? $rule->safeParse($data) : $rule->parse($data);
 
             $validationResult[$key] = [
                 'value' => $parseResult->getValue(),
                 'valid' => $parseResult->isValid(),
                 'errors' => $parseResult->getErrors(),
             ];
-
         }
 
         return new ValidationResponse($validationResult);
